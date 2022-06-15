381ddb42540ec599a7d63b91e70409206292a26d
Brett Torra <brett.cs19@gmail.com>
2022-06-15 13:19:38 -0400

    all initial dashes in one array

--- scraper.js
+++ scraper.js
@@ -22,14 +22,20 @@ const pullInitialDashData = async () => {
     const $ = cheerio.load(rawData);
     $('#AutoNumber1 tbody tr').each(function (i, elm) {
         initialDashes.push($(this).text().trim());
-        let string = initialDashes[i].replace(/\s/g, ' ');
-        string = string.replace(/\s+/g, ' ');
-        string = string.replace(/[0-9]*\-[0-9]+|^[0-9]+/, '');
+        let name = initialDashes[i].replace(/\s/g, ' '); // remove endlines
+        let value = initialDashes[i].replace(/\s/g, ' '); // remove endlines
+        name = name.replace(/\s+/g, ''); // remove white spaces
+        name = name.replace(/[0-9]*\-[0-9]+|^[0-9]+/, ''); //remove beginning numbers
+        value = value.replace(/\s+/g, ''); // remove white spaces
+        value = value.replace(/[0-9]*\-[0-9]+|^[0-9]+/, ''); //remove beginning numbers
+
+        let currentCharacter = [];
+        name = name.replace(/[0-9].+/, '');
+        value = value.replace(/^[A-Z]([A-Z]|[a-z]|\D)+/, ''); // remove names
+        value = value.replace(/[A-Z].+/, ''); // remove special cases
+        currentCharacter = [name, value];
+        initialDash.push(currentCharacter);
     });
-
-
-   
 }
-
-
+let initialDash = [];
 pullInitialDashData();
