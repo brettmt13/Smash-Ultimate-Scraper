53d8a1b570a7950ec1e0e3ec01be3ace266c1db3
Brett Torra <brett.cs19@gmail.com>
2022-08-02 19:35:58 -0400

    flex box banner structure complete

--- app/src/Banner.js
+++ app/src/Banner.js
@@ -4,12 +4,16 @@ import './Banner.css';
 function Banner() {
     return (
         <div className="Banner">
-            <div>
-                <ul className="Menu">
-                    <li>Stats</li>
-                    <li>Compare</li>
-                </ul>
-            </div>
+            
+            <ul className = "Menu">
+                <li>Stats</li>
+                <li>Compare</li>
+            </ul>
+            
+            <span className="Title">
+                <h1>SSBU Visual</h1>
+                <h3>A Frame Data Visualisation</h3>
+             </span>
          </div>
     )
 }
