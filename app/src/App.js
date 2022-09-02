c4c04269d7823fb315ea9114c783877cb9cb7331
Brett Torra <brett.cs19@gmail.com>
2022-09-02 15:22:15 -0400

    create app and add a banner

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Banner from './Banner';
+
+function App() {
+  return (
+      <div>
+          <Banner />
+      </div>
+  );
+}
+
+export default App;
---
+++ app/src/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="Banner">
+                <h1>SSBU Visual</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
